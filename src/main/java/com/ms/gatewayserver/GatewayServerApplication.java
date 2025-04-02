6b5ba984d579b8ef7389efc1779748f80459789c
Manuel Silva <manuel.silvag1@gmail.com>
2025-04-02 00:24:27 -0500

    Added custom routing configuration

--- src/main/java/com/ms/gatewayserver/GatewayServerApplication.java
+++ src/main/java/com/ms/gatewayserver/GatewayServerApplication.java
@@ -2,12 +2,43 @@ package com.ms.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder
+				.routes()
+				.route(
+						predicateSpec -> predicateSpec
+								.path("/ms-bank/accounts/**")
+								.filters(gatewayFilterSpec -> gatewayFilterSpec
+										.rewritePath("/ms-bank/accounts/?(?<segment>.*)", "/${segment}"))
+								.uri("lb://ACCOUNTS")
+				)
+				.route(
+						predicateSpec -> predicateSpec
+								.path("/ms-bank/cards/**")
+								.filters(gatewayFilterSpec -> gatewayFilterSpec
+										.rewritePath("/ms-bank/cards/?(?<segment>.*)", "/${segment}"))
+								.uri("lb://CARDS")
+				)
+				.route(
+						predicateSpec -> predicateSpec
+								.path("/ms-bank/loans/**")
+								.filters(gatewayFilterSpec -> gatewayFilterSpec
+										.rewritePath("/ms-bank/loans/?(?<segment>.*)", "/${segment}"))
+								.uri("lb://LOANS")
+				)
+				.build();
 	}
 
 }
